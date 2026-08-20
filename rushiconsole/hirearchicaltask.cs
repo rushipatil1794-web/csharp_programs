804a647df84677998843c3d2b327d7cd8314bb60
rushipatil <rushipatil1794@gmail.com>
2026-08-20 15:28:02 -0400

    inheritance tasks

---
+++ rushiconsole/hirearchicaltask.cs
@@ -0,0 +1,59 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace rushiconsole
+//{
+//    class institude // parent class
+//    {
+//        public void pune_institude()
+//        {
+//            Console.WriteLine("1.Fortune Cloude");
+//            Console.WriteLine("2.Qspyder");
+//            Console.WriteLine("3.Up Skills");
+
+//        }
+
+//    }
+//    class fortunecloude:institude first child class of institude
+//    {
+//        public void fortune_course()
+//        {
+//            Console.WriteLine("1:java fullstack");
+//            Console.WriteLine("2..net fullstack");
+//            Console.WriteLine("3.Up python fullstack");
+//            Console.WriteLine("4.cyber secqurity");
+//            Console.WriteLine("5.aws cloude");
+//            Console.WriteLine("6.Up da/ds");
+//        }
+//    }
+//    class qspyder : institude //second child class of institude
+//    {
+//        public void qspde_course()
+//        {
+//            Console.WriteLine("1:java fullstack");
+//            Console.WriteLine("2.Up python fullstack");
+//            Console.WriteLine("3.cyber secqurity");
+//            Console.WriteLine("4.Up da/ds");
+//        }
+//    }
+//    internal class hirearchicaltask
+//    {
+//        public static void Main(string[] args)
+//        {
+//            Console.WriteLine("Create fortune cloude object");
+//            fortunecloude f=new fortunecloude();
+//            Console.WriteLine("Insitude Names:");
+//            f.pune_institude();
+//            Console.WriteLine("Prrovide coureses:");
+//            f.fortune_course();
+//            Console.WriteLine("____________________________");
+//            Console.WriteLine("Create fortune cloude object");
+//            qspyder qspyder = new qspyder();
+//            Console.WriteLine("Insitude Names:");
+//            qspyder.pune_institude();
+//            Console.WriteLine("Prrovide coureses:");
+//            qspyder.qspde_course();
+//        }
+//    }
+//}
---
+++ rushiconsole/multilevaltask.cs
@@ -0,0 +1,47 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace rushiconsole
+//{
+//    class country // parent class
+//    {
+//        public void show_country()
+//        {
+//            Console.WriteLine("India");
+//            Console.WriteLine("America");
+//            Console.WriteLine("Russia");
+//        }
+//    }
+//    class state:country // child class of country
+//    {
+//        public void show_state()
+//        {
+//            Console.WriteLine("Maharashtra");
+//            Console.WriteLine("Karnataka");
+//            Console.WriteLine("Bihar");
+//        }
+//    }
+//    class city:state // child class of state
+//    {
+//        public void show_punespecial()
+//        {
+//            Console.WriteLine("Iskon Temple");
+//            Console.WriteLine("Shremant Dagadushet");
+//            Console.WriteLine("Sarasbag");
+//        }
+//    }
+//    internal class multilevaltask
+//    {
+//        public static void Main(string[] args)
+//        {
+//            city c=new city();
+//            Console.WriteLine("Countris:");
+//            c.show_country();
+//            Console.WriteLine("States In Inadia:");
+//            c.show_state();
+//            Console.WriteLine("Pune City Special");
+//            c.show_punespecial();
+//        }
+//    }
+//}
