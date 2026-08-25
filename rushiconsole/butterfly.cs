b19cb7c39b3658d2890eb76b727f09a60b30cc75
rushipatil <rushipatil1794@gmail.com>
2026-08-25 11:28:19 -0400

    patterns

---
+++ rushiconsole/butterfly.cs
@@ -0,0 +1,48 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace rushiconsole
+//{
+//    internal class butterfly
+//    {
+//        public static void Main(string[] args)
+//        {
+//            int i, j, n;
+//            Console.Write("Enter the number of rows: ");
+//            n = Convert.ToInt32(Console.ReadLine());
+//            for (i = 1; i <= n; i++)
+//            {
+//                for (j = 1; j <= i; j++)
+//                {
+//                    Console.Write("*");
+//                }
+//                for (j = 1; j <= 2 * (n - i); j++)
+//                {
+//                    Console.Write(" ");
+//                }
+//                for (j = 1; j <= i; j++)
+//                {
+//                    Console.Write("*");
+//                }
+//                Console.WriteLine();
+//            }
+//            for (i = n; i >= 1; i--)
+//            {
+//                for (j = 1; j <= i; j++)
+//                {
+//                    Console.Write("*");
+//                }
+//                for (j = 1; j <= 2 * (n - i); j++)
+//                {
+//                    Console.Write(" ");
+//                }
+//                for (j = 1; j <= i; j++)
+//                {
+//                    Console.Write("*");
+//                }
+//                Console.WriteLine();
+//            }
+//        }
+//    }
+//}
---
+++ rushiconsole/heart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace rushiconsole
+{
+    internal class heart
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
