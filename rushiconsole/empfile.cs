11d2d379d7ef3deb215a5e09814ff1d9cedb2ea3
rushipatil <rushipatil1794@gmail.com>
2026-08-28 20:05:09 -0400

    filehandling

---
+++ rushiconsole/empfile.cs
@@ -0,0 +1,55 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace rushiconsole
+//{
+//    class empwrite
+//    {
+//        public void task_write(int empId, string empName,string empdesignation,string education,double empSalary)
+//        {
+//        FileStream fs = new FileStream("C:\\Users\\Public\\employe.txt", FileMode.OpenOrCreate);
+//        StreamWriter sw = new StreamWriter(fs);
+//        sw.WriteLine($"Employee ID: {empId}");
+//        sw.WriteLine($"Employee Name: {empName}");
+//        sw.WriteLine($"Employee Designation: {empdesignation}");
+//        sw.WriteLine($"Employee Education: {education}");
+//        sw.WriteLine($"Employee Salary: {empSalary}");
+//        sw.Close();
+//        fs.Close();
+//        }
+//    }
+//    class empread : empwrite
+//    {
+//        public void task_read()
+//        {
+//            FileStream fs = new FileStream("C:\\Users\\Public\\employe.txt", FileMode.Open);
+//            StreamReader sr = new StreamReader(fs);
+//            string line = sr.ReadToEnd();
+//            Console.WriteLine(line);
+//            sr.Close();
+//            fs.Close();
+//        }
+//    }
+//    internal class empfile:empread
+//    {
+//        public static void Main(string[] args)
+//        {
+//            Console.WriteLine("Employee Information");
+//            Console.WriteLine("--------------------");
+//            Console.WriteLine("Enter Employee ID:");
+//            int empId=int.Parse(Console.ReadLine());
+//            Console.WriteLine("Enter Employee Name:");
+//            string empName=Console.ReadLine();
+//            Console.WriteLine("Enter Employee Designation:");
+//            string empdesignation=Console.ReadLine();
+//            Console.WriteLine("Enter Employee Education:");
+//            string education=Console.ReadLine();
+//            Console.WriteLine("Enter Employee Salary:");
+//            double empSalary= double.Parse(Console.ReadLine());
+//            empfile ef = new empfile();
+//            ef.task_write(empId, empName, empdesignation, education, empSalary);
+//            ef.task_read();
+//        }
+//    }
+//}
--- rushiconsole/filehandling.cs
+++ rushiconsole/filehandling.cs
@@ -1,71 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace rushiconsole
-{
-    class read
-    {
-        public void task_read()
-        {
-            FileStream fs = new FileStream("C:\\Users\\Public\\rushi.txt", FileMode.OpenOrCreate);
-            StreamReader sr = new StreamReader(fs);
-            string line=sr.ReadToEnd();
-            Console.WriteLine(line);
-            sr.Close();
-            fs.Close();
+//namespace rushiconsole
+//{
+//    class read
+//    {
+//        public void task_read()
+//        {
+//            FileStream fs = new FileStream("C:\\Users\\Public\\rushi.txt", FileMode.OpenOrCreate);
+//            StreamReader sr = new StreamReader(fs);
+//            string line=sr.ReadToEnd();
+//            Console.WriteLine(line);
+//            sr.Close();
+//            fs.Close();
 
-        }
-    }
-    class write: read
-    {
-        public void task_write()
-        {
-            FileStream fs = new FileStream("C:\\Users\\Public\\rushi.txt", FileMode.OpenOrCreate);
-            StreamWriter sw = new StreamWriter(fs);
-            sw.WriteLine("This is a sample text written to the file.");
-            sw.WriteLine("You can write multiple lines of text.");
-            sw.Close();
-            fs.Close();
-        }
-    }
-    internal class filehandling:write
-    {
+//        }
+//    }
+//    class write: read
+//    {
+//        public void task_write()
+//        {
+//            FileStream fs = new FileStream("C:\\Users\\Public\\rushi.txt", FileMode.OpenOrCreate);
+//            StreamWriter sw = new StreamWriter(fs);
+//            sw.WriteLine("This is a sample text written to the file.");
+//            sw.WriteLine("You can write multiple lines of text.");
+//            sw.Close();
+//            fs.Close();
+//        }
+//    }
+//    internal class filehandling:write
+//    {
         
-        public static void Main(string[] args)
-        {
-            // call without inheritance
-            //write w = new write();
-            //w.task_file();
-            // call with inheritance
-            filehandling fh = new filehandling();
-            fh.task_write();
-            fh.task_read();
-            //string Path = "C:\\Users\\Public\\rushi.txt";
-            //// Write to a file
-            //File.Create(Path);
-            //// file open
-            //if(File.Exists(Path))
-            //{
-            //    Console.WriteLine("File is created");
+//        public static void Main(string[] args)
+//        {
+//            // call without inheritance
+//            //write w = new write();
+//            //w.task_file();
+//            // call with inheritance
+//            filehandling fh = new filehandling();
+//            fh.task_write();
+//            fh.task_read();
+//            //string Path = "C:\\Users\\Public\\rushi.txt";
+//            //// Write to a file
+//            //File.Create(Path);
+//            //// file open
+//            //if(File.Exists(Path))
+//            //{
+//            //    Console.WriteLine("File is created");
 
-            //}
-            //else
-            //{
-            //    Console.WriteLine("File is not created");
-            //}
-            ////delete operation
-            //string location = "C:\\Users\\Public\\rushikesh";
-            //if(File.Exists(location))
-            //{
-            //    File.Delete(location);
-            //    Console.WriteLine("File is deleted");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("File is not deleted ");
-            //    File.Create(location);
-            //}
-        }
-    }
-}
+//            //}
+//            //else
+//            //{
+//            //    Console.WriteLine("File is not created");
+//            //}
+//            ////delete operation
+//            //string location = "C:\\Users\\Public\\rushikesh";
+//            //if(File.Exists(location))
+//            //{
+//            //    File.Delete(location);
+//            //    Console.WriteLine("File is deleted");
+//            //}
+//            //else
+//            //{
+//            //    Console.WriteLine("File is not deleted ");
+//            //    File.Create(location);
+//            //}
+//        }
+//    }
+//}
