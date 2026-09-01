690cff8968b28e1667cd204ce58e3d31104c59dc
rushipatil <rushipatil1794@gmail.com>
2026-09-01 14:26:54 -0400

    thread properties

--- rushiconsole/multithreads.cs
+++ rushiconsole/multithreads.cs
@@ -1,49 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace rushiconsole
-{
+//namespace rushiconsole
+//{
     
-    internal class multithreads
-    {
-        public static void crushtask()
-        {
-            Console.WriteLine("here candy crush is running");
-            Console.WriteLine("thread 1 is executing task....." + Thread.CurrentThread);
-        }
-        public static void crussound()
-        {
-            Console.WriteLine("here candy crush sound is running");
+//    internal class multithreads
+//    {
+//        public static void crushtask()
+//        {
+//            Console.WriteLine("here candy crush is running");
+//            Console.WriteLine("thread 1 is executing task....." + Thread.CurrentThread);
+//        }
+//        public static void crussound()
+//        {
+//            Console.WriteLine("here candy crush sound is running");
             
-        }
-        public static void multithreadtask()
-        {
-            crushtask();
-            crussound();
+//        }
+//        public static void multithreadtask()
+//        {
+//            crushtask();
+//            crussound();
             
-        }
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Main thread is executing task....." + Thread.CurrentThread);
-            Console.WriteLine("using single thread to single task");
-            Thread t1 = new Thread(crushtask);
-            t1.Start();
-            Console.WriteLine("using single thread to multiple task");
-            Thread t2 = new Thread(multithreadtask);
-            t2.Start();
-            Console.WriteLine("using multiple threads to single task");
-            Thread t3 = new Thread(crushtask);
-            Thread t4 = new Thread(crushtask);
-            t3.Start();
-            t4.Start();
-            Console.WriteLine("using multiple threads to multiple task");
-            Thread t5 = new Thread(crushtask);
-            Thread t6 = new Thread(crussound);
-            t5.Start();
-            t6.Start();
+//        }
+//        public static void Main(string[] args)
+//        {
+//            //Console.WriteLine("Main thread is executing task....." + Thread.CurrentThread);
+//            //Console.WriteLine("using single thread to single task");
+//            //Thread t1 = new Thread(crushtask);
+//            //t1.Start();
+//            //Console.WriteLine("using single thread to multiple task");
+//            //Thread t2 = new Thread(multithreadtask);
+//            //t2.Start();
+//            Console.WriteLine("using multiple threads to single task");
+//            Thread t3 = new Thread(crushtask);
+//            Thread t4 = new Thread(crushtask);
+//            t3.Start();
+//            t4.Start();
+//            ////Console.WriteLine("using multiple threads to multiple task");
+//            ////Thread t5 = new Thread(crushtask);
+//            ////Thread t6 = new Thread(crussound);
+//            ////t5.Start();
+//            ////t6.Start();
 
 
-        }
-    }
-}
+//        }
+//    }
+//}
---
+++ rushiconsole/threadtask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace rushiconsole
+{
+    internal class threadtask
+    {
+        public  void task1()
+        {
+            Console.WriteLine("task1 is running");
+        }
+        public static void Main(string[] args)
+        {
+            threadtask tt = new threadtask();
+            Console.WriteLine("curent thread is " + Thread.CurrentThread);
+            Thread t1 = new Thread(tt.task1);
+            t1.Name = "thread1";
+            t1.Priority = ThreadPriority.BelowNormal;
+            Console.WriteLine("thread name is " + t1.Name);
+            Console.WriteLine("thread priority is " + t1.Priority);
+            Console.WriteLine("thread state is " + t1.ThreadState);
+        }
+    }
+}
